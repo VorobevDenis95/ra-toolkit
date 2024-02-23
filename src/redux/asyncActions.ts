af13941538e8b66f4f7bfffd8b58365b453f11ef
Denis <kurtcobaine192@outlook.com>
2024-02-23 11:07:07 +0300

    type asyncActions

--- src/redux/asyncActions.ts
+++ src/redux/asyncActions.ts
@@ -1,29 +1,34 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchFilms = createAsyncThunk(
+export const fetchFilms = createAsyncThunk (
   'films/fetchFilms',
-  async function (title:string, {rejectWithValue}){
+  async function (title:string, thunkAPI){
     try {
       const response = await fetch(`https://www.omdbapi.com/?apikey=9713c5e7&s=${title}`);
       if (!response.ok) throw new Error('Servel Error!');
       const data = await response.json();
       return data;
     } catch (error) {
-      return rejectWithValue(error.message);
+      if (error instanceof Error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+      return thunkAPI.rejectWithValue('Unknown error');
     }
   }
 )
 
 export const fetchAboutFilm = createAsyncThunk(
   'films/fetchAboutFilm',
-  async function (id: string, {rejectWithValue}){
+  async function (id: string, thunkAPI){
     try {
       const response = await fetch(`https://www.omdbapi.com/?apikey=9713c5e7&i=${id}`);
       if (!response.ok) throw new Error('Servel Error!');
       const data = await response.json();
       return data ;
     } catch (error) {
-      return rejectWithValue(error.message);
+      if (error instanceof Error) 
+      return thunkAPI.rejectWithValue(error.message);
+    return thunkAPI.rejectWithValue('Unknown error');
     }
   }
 )
